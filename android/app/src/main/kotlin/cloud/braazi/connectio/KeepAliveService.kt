a599ba5d2bfa6f2989fd896204c17868c7c3af74
Sergio Baiao <sergiobaiao@gmail.com>
2026-04-27 19:21:10 -0300

    Sync: Updated dev with all changes from main (files state sync)

---
+++ android/app/src/main/kotlin/cloud/braazi/connectio/KeepAliveService.kt
@@ -0,0 +1,16 @@
+package cloud.braazi.connectio
+
+import android.app.Service
+import android.content.Intent
+import android.os.Binder
+import android.os.IBinder
+
+class KeepAliveService: Service() {
+  companion object {
+    val binder = Binder()
+  }
+
+  override fun onBind(intent: Intent): IBinder {
+    return binder
+  }
+}
